73ba4279279ff6382daaf16aaad2d4896782b505
km7865 <51434817+km7865@users.noreply.github.com>
2020-10-17 10:17:02 +0900

    manager 레시피 등록 함수 추가

--- manager.js
+++ manager.js
@@ -103,3 +103,39 @@ exports.login = function (req, res) {
         });
     });
 }
+
+exports.createRecipe = function (req, res) {
+    console.log('who get in here post /createRecipe');
+    var buffers = []
+    var inputData;
+
+    req.on('data', (data) => {
+        buffers.push(data); // 이미지 byte를 여러 번에 걸쳐서 buffer에 저장
+    });
+
+    req.on('end', () => {
+        inputData = JSON.parse(Buffer.concat(buffers).toString()); // buffer 배열을 합친 후 parse
+        console.log("title : "+inputData.title + " , ingredient : " + inputData.ingredient);
+
+        var fs = require('fs'); //File System 모듈 불러오기
+
+        // 전송된 이미지 갯수만큼 아래 경로에 저장
+        // 파일명 : 레시피Id + 숫자(대표이미지 : 0, 조리법이미지 : 1~10)
+        // 데이터베이스 저장 코드는 구현 필요
+        for (var i = 0; i < inputData.recipeImageCount; i++) {
+            var bitmap = Buffer.from(inputData["recipeImageByte" + i], 'base64');
+            var filePath = './img/recipeId' + i + '.png';
+            fs.writeFile(filePath, bitmap, (err) => {
+                if (err) {
+                    throw err;
+                    console.log('write Failed!');
+                }
+                else console.log('write Complete!');
+            });
+        }
+
+        // dbConnector.createRecipe
+        res.write("ok");
+        res.end();
+    });
+}
