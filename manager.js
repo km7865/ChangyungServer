1fbc879cfc8b6e50217940bcecf2f9784a116023
km7865 <51434817+km7865@users.noreply.github.com>
2020-10-25 15:05:40 +0900

    중간2

--- manager.js
+++ manager.js
@@ -216,14 +216,97 @@ exports.signUp = function (req, res) {
     });
 }
 
+// 회원 정보 삭제
 exports.deleteUser = function (req, res) {
+    console.log('who get in here post /deleteUser');
+    var inputData;
+
+    req.on('data', (data) => {
+        inputData = JSON.parse(data);
+    });
+
+    req.on('end', () => {
+        console.log("id : " + inputData.userId);
+
+        db.deleteUser(inputData.userId, (results) => {
+            res.write(results);
+            res.end();
+        });
+    });
 }
 
+// 회원 정보 수정
 exports.updateUser = function (req, res) {
+    console.log('who get in here post /updateUser');
+    var inputData;
+
+    req.on('data', (data) => {
+        inputData = JSON.parse(data);
+    });
+
+    req.on('end', () => {
+        console.log("id : " + inputData.userId + " pw : " + inputData.pw);
+
+        db.updateUser(inputData.userId, inputData.pw, (results) => {
+            res.write(results);
+            res.end();
+        });
+    });
 }
 
+// 레시피 등록
 exports.createRecipe = function (req, res) {
     console.log('who get in here post /createRecipe');
+    var buffers = []
+    var inputData;
+
+    req.on('data', (data) => {
+        buffers.push(data); // 레시피정보를 여러 번에 걸쳐서 buffer에 저장 (이미지 byte가 매우 크기 때문에)
+    });
+
+    req.on('end', () => {
+        inputData = JSON.parse(Buffer.concat(buffers).toString()); // buffer 배열을 합친 후 parse
+
+        // 이미지경로 : 해당 프로젝트 경로의 img 디렉토리 + 레시피Id + 숫자(대표이미지 : 0, 조리법이미지 : 1~10)
+        // img 디렉토리가 없다면 생성 필요
+        // 1. db.createRecipe() 호출로 이미지 경로를 제외한 정보 삽입 후 callback으로 recipeInId Get
+        // 2. 얻은 recipeInId로 이미지 경로 설정 및 저장
+        // 3. db.updateImgPath() 호출로 이미지 경로 정보 갱신
+        db.createRecipe(inputData.userId, inputData.title, inputData.ingredient, inputData.ingredientUnit,
+            inputData.recipePerson, inputData.recipeTime, inputData.contents, (results) => {
+                if (results == "2") {
+                    res.write(results);
+                    res.end();
+                } else {
+                    var recipeInId = results;
+                    var imgPaths = "";
+                    var fs = require('fs'); //File System 모듈 불러오기
+                    var imgArr = JSON.parse(inputData["recipeImageBytes"]);
+                    for (var i = 0; i < Object.keys(imgArr).length; i++) {
+                        var bitmap = Buffer.from(imgArr[i]["recipeImageByte"], 'base64');
+                        var imgPath = './img/' + recipeInId + '_' + i + '.png';
+                        if (i) imgPaths += '`';
+                        imgPaths += imgPath;
+                        fs.writeFile(imgPath, bitmap, (err) => {
+                            if (err) {
+                                throw err;
+                                console.log('write Failed!');
+                            } else console.log('write Complete!');
+                        });
+                    }
+
+                    db.updateImgPath(recipeInId, imgPaths, (results) => {
+                        res.write(results);
+                        res.end();
+                    });
+                }
+            });
+    });
+}
+
+// 레시피 수정
+exports.updateRecipe = function (req, res) {
+    console.log('who get in here post /updateRecipe');
     var buffers = []
     var inputData;
 
@@ -233,42 +316,58 @@ exports.createRecipe = function (req, res) {
 
     req.on('end', () => {
         inputData = JSON.parse(Buffer.concat(buffers).toString()); // buffer 배열을 합친 후 parse
-        console.log("title : "+inputData.title + " , ingredient : " + inputData.ingredient);
 
-        var fs = require('fs'); //File System 모듈 불러오기
-
-        // 전송된 이미지 갯수만큼 아래 경로에 저장
-        // 파일명 : 레시피Id + 숫자(대표이미지 : 0, 조리법이미지 : 1~10)
-        // 데이터베이스 저장 코드는 구현 필요
         var imgPaths = "";
-
+        var fs = require('fs'); //File System 모듈 불러오기
         var imgArr = JSON.parse(inputData["recipeImageBytes"]);
         for (var i = 0; i < Object.keys(imgArr).length; i++) {
             var bitmap = Buffer.from(imgArr[i]["recipeImageByte"], 'base64');
-            var imgPath = './img/recipeId' + i + '.png';
-            if (i) imgPaths += ',';
+            var imgPath = './img/' + recipeInId + '_' + i + '.png';
+            if (i) imgPaths += '`';
             imgPaths += imgPath;
             fs.writeFile(imgPath, bitmap, (err) => {
                 if (err) {
                     throw err;
-                    console.log('write Failed!');
                 } else console.log('write Complete!');
             });
         }
 
-        // 이미지경로 - 변수 imgPaths
-        // dbConnector.readRecipe
-        res.write("ok");
-        res.end();
+        db.updateRecipe(inputData.recipeInId, inputData.title, inputData.ingredient, inputData.ingredientUnit,
+            inputData.recipePerson, inputData.recipeTime, inputData.contents, imgPaths, (results) => {
+                res.write(results);
+                res.end();
+            });
     });
 }
 
-exports.updateRecipe = function (req, res) {
-}
-
+// 레시
 exports.deleteRecipe = function (req, res) {
+    console.log('who get in here post /deleteRecipe');
+    var inputData;
+
+    req.on('data', (data) => {
+        inputData = JSON.parse(data);
+    });
+
+    req.on('end', () => {
+        var fs = require('fs'); //File System 모듈 불러오기
+
+        db.readImgPath(inputData.recipeInId, (results) => {
+            console.log(results);
+            var imgPaths = results["imgPath"].split('`');
+            for (var i = 0; i < imgPaths.length; i++) {
+                fs.unlinkSync(imgPaths[i]);
+            }
+        });
+
+        db.deleteRecipe(inputData.recipeInId, (results) => {
+            res.write(results);
+            res.end();
+        });
+    });
 }
 
+// 레시피 조회
 exports.readRecipe = function (req, res) {
     console.log('who get in here post /readRecipe');
     var inputData;
@@ -278,17 +377,30 @@ exports.readRecipe = function (req, res) {
     });
 
     req.on('end', () => {
-        console.log(inputData.userId);
+        console.log(inputData.type);
 
         var fs = require('fs'); //File System 모듈 불러오기
 
-        var recipeImageByte = fs.readFileSync('./img/recipeId0.png', 'base64');
-        console.log(recipeImageByte);
-
-
-        // dbConnector.readRecipe
-        res.write("ok");
-        res.end();
+        db.readRecipe((results) => {
+            if (results == "2") {
+                res.write(results);
+            } else {
+                var recipeArr = results;
+                var recipeImageBytes = [];
+                for (var i = 0; i < recipeArr.length; i++) {
+                    var imgPaths = recipeArr[i]["imgPath"].split('`');
+                    for (var j = 0; j < imgPaths.length; j++) {
+                        var imgPath = new Object();
+                        imgPath.recipeImageByte = fs.readFileSync(imgPaths[j], 'base64');
+                        recipeImageBytes.push(imgPath);
+                    }
+                    recipeArr[i].recipeImageBytes = recipeImageBytes;
+                    delete recipeArr[i].imgPath;
+                }
+                res.write(JSON.stringify(recipeArr));
+            }
+            res.end();
+        });
     });
 }
 
